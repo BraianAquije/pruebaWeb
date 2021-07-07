77a77938a5b78d50b51a42e54aa8a9e917cf75a1
Braian <braian.aquije@gmail.com>
2021-07-07 18:14:11 -0300

    se modificaron colores

--- prueba.php
+++ prueba.php
@@ -1,6 +1,6 @@
 <?php
 
-echo ("<h2 style='color:red;'> LIONEL MESSI </h2>");
+echo ("<h2 style='color:blue;'> LIONEL MESSI </h2>");
 
 echo ("<h2 style='color:orange;'> El mejor jugador del mundo </h1>");
 
